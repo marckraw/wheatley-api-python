c11403bc9e80b60afde8da51a5a58e57e34e87be
Marcin Krawczyk <marckraw@icloud.com>
2021-12-14 00:24:52 +0100

    Solve cors stuff, add localhost and wheatley production

--- wheatley_api/wheatley_api/settings.py
+++ wheatley_api/wheatley_api/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
     'todos.apps.TodosConfig',
     'api.apps.ApiConfig',
 
-    'rest_framework'
+    'rest_framework',
+
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://wheatley.netlify.app"
+]
