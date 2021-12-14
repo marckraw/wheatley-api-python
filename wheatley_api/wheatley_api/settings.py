3dfbf836f559d286f2f34144f45d238432a55833
Marcin Krawczyk <marckraw@icloud.com>
2021-12-14 11:48:11 +0100

    Fix collect static thing

--- wheatley_api/wheatley_api/settings.py
+++ wheatley_api/wheatley_api/settings.py
@@ -138,7 +138,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
