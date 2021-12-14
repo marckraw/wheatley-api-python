f084a80642edba03a11131de482e48e6ecbe5a94
Marcin Krawczyk <marckraw@icloud.com>
2021-12-14 11:50:47 +0100

    Fix static root

--- wheatley_api/wheatley_api/settings.py
+++ wheatley_api/wheatley_api/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
